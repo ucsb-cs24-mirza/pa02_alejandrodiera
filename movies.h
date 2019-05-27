47b3857a73e9b75300a703b87f234a65aa880b5e
Alejandro Diera <alejandrodiera@ucsb.edu>
2019-05-26 21:22:05 -0700

    Updating

--- movies.h
+++ movies.h
@@ -10,8 +10,25 @@ using namespace std;
 
 class BST {
 
+public:
 
+    BST();
+    ~BST();
+    bool insert(int value);
+    bool contain(int value);
+    bool remove(int value);
 
+private:
+
+    struct Node {
+      int info;
+      Node *left, *right, *parent;
+      Node(int v=0) : info(v), left(0), right(0) parent(0) {}
+    };
+    Node *root;
+    Node* getNodeFor(int value, Node* n)const;
+    void clear(Node *n);
+    bool insert(int value, Node *n);
 
 };
 
